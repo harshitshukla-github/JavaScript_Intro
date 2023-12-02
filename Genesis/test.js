130313b5ff788e21a3e94ae777d607c4859eba25
HARSHIT SHUKLA <32368309+harshitshukla-github@users.noreply.github.com>
2023-12-02 03:24:15 +0000

    Chapter-I:- var, let & const
    Update JavaScript code to use strict mode and introduce variable
    declaration. (#1234)
    
    Enable the use of strict mode in the JavaScript code to enforce stricter
    syntax rules and avoid common mistakes. Declare variables using the
    `const` keyword to ensure immutability and prevent accidental
    reassignment. Provide examples and explanations of variable declaration
    using `const`. This change improves code reliability and readability.
    
    Resolves #1234.

--- Genesis/test.js
+++ Genesis/test.js
@@ -1,1 +1,12 @@
+"use strict" // Treat all JS Code as newer version
 console.log("Hello CodeSpaces");
+// Chapter-I:- var, let, const
+
+const a_string = "Hello"
+
+another_string = "Something Different"
+
+// a_string += "World!" - TypeError
+console.table([a_string, another_string])
+
+//Chapter-II:- Datatypes
