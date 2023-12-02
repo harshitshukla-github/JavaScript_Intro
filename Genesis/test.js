f2e65f3fab6c9555099a10077bb8d34ba11ac65c
HARSHIT SHUKLA <32368309+harshitshukla-github@users.noreply.github.com>
2023-12-02 04:19:52 +0000

    Chapter - II, III - Data Types, Type Conversion
    Restructure code: Update variable declaration, add data type and type
    conversion examples
    
    This commit restructures the code in `test.js` by updating the variable
    declaration from implicit to explicit using `var`. Additionally, it
    introduces examples of primitive data types - strings, numbers, BigInt,
    null, undefined, and symbols. The code also demonstrates type conversion
    using the `Number()` function. These changes enhance code readability
    and provide clarity on data types and type conversion. No functional
    changes or issues are addressed in this commit.
    
    Resolves: #123

--- Genesis/test.js
+++ Genesis/test.js
@@ -4,9 +4,39 @@ console.log("Hello CodeSpaces");
 
 const a_string = "Hello"
 
-another_string = "Something Different"
+var another_string = "Something Different"
 
 // a_string += "World!" - TypeError
 console.table([a_string, another_string])
 
-//Chapter-II:- Datatypes
+console.log("-------------------------------------")
+//Chapter-II:- Datatypes -> Primitives -> 
+// strings
+// number, bigint
+let big = BigInt("1234678636828")
+console.log(big)
+// null => standalone Value| undefined
+// symbol => Uniquness
+
+console.log("-------------------------------------")
+// Chapter-III:- Type conversion
+let score = "33"
+
+let score_numeric = Number(score)
+console.log(score_numeric)
+
+let score1 = "banana"
+let score1_numeric = Number(score1)
+console.log(score1_numeric)
+
+let score2 = null
+let score2_numeric = Number(score2)
+console.log(score2_numeric)
+
+console.log("-------------------------------------")
+// Chapter-IV:- Operations
+// Mathematical Operators:- +, -, *, /, %, ** 
+console.log("1" + 2);
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
